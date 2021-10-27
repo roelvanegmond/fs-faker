0e0d6309a12fd24b823ac4ab3e1b77d4469c959b
roelvanegmond <36486184+roelvanegmond@users.noreply.github.com>
2021-10-27 12:32:51 +0200

    Add option to delete, updated defaults, restructure, add error handling.

--- index.js
+++ index.js
@@ -1,76 +1,165 @@
-const config = require('config');
-const fs = require('fs');
-const faker = require('faker');
-const fakeFile = require('fake-file-generator');
+const config = require("config");
+const fs = require("fs");
+const faker = require("faker");
+const fakeFile = require("fake-file-generator");
+const progress = require("cli-progress");
+const chalk = require("chalk");
+const program = require("commander").program;
+const prompt = require("prompt");
 
 let directories = [];
 
-// generate directory paths and create them recursively
-let dirCount = 0;
-while(dirCount < config.get('maxUniqueDirPaths')){
+program
+  .version("1.0.0")
+  .option(
+    "-d, --delete",
+    "delete the data structure before generating a new one"
+  );
 
-    const directoryPath = config.get('dataRoot') + faker.fake(generateDirectoryPath(config.get('pathConstructor'), config.get('maxDepth')));
-    if(!fs.existsSync(directoryPath)){
+program.parse(process.argv);
+
+let options = program.opts();
+
+run();
+
+async function run() {
+  if (options.delete) {
+    prompt.start();
+
+    var schema = {
+      properties: {
+        delete: {
+          message:
+            chalk.yellow("About to delete " +
+            config.get("dataRoot") +
+            " and all it's contents. Continue?"),
+          required: true,
+        },
+      },
+    };
+
+    prompt.get(schema, function (err, result) {
+      if (result.delete.toLowerCase().trim() == "y") {
+        console.log("Deleting " + config.get("dataRoot"));
+        fs.rmdirSync(config.get("dataRoot"), { recursive: true });
+      }
+      else{
+          generateAndShowProgress();
+      }
+    });
+  } else {
+      generateAndShowProgress();
+  }
+}
+
+async function generateAndShowProgress() {
+  // create a new progress bar instance and use shades_classic theme
+  const multibar = new progress.MultiBar(
+    {
+      clearOnComplete: false,
+      hideCursor: true,
+      format: "{title} [{bar}] {percentage}% | ETA: {eta}s | {value}/{total}",
+    },
+    progress.Presets.shades_grey
+  );
+
+  // add bars
+  const dirBar = multibar.create(config.get("maxUniqueDirPaths"), 0, {
+    title: "Creating directories... ",
+  });
+  const fileBar = multibar.create(config.get("maxFileCount"), 0, {
+    title: "Creating files...       ",
+  });
+
+  await generateDirectoryStructure(dirBar);
+  await generateFiles(fileBar);
+
+  // stop all bars
+  multibar.stop();
+}
+
+// generate directory paths and create them recursively
+async function generateDirectoryStructure(dirBar) {
+  let dirCount = 0;
+  while (dirCount < config.get("maxUniqueDirPaths")) {
+    const directoryPath =
+      config.get("dataRoot") +
+      faker.fake(
+        generateDirectoryPath(
+          config.get("pathConstructor"),
+          config.get("maxDepth")
+        )
+      );
+    try {
+      if (!fs.existsSync(directoryPath)) {
         fs.mkdirSync(directoryPath, { recursive: true });
+      }
+    } catch (e) {
+      console.log(chalk.red(e.message));
     }
 
     directories.push(directoryPath);
-
     dirCount++;
+    dirBar.update(dirCount);
+  }
 }
 
 // randomly pick a path from the directories create a file
-let fileCount = 0;
-while(fileCount < config.get('maxFileCount')){
-
+async function generateFiles(fileBar) {
+  let fileCount = 0;
+  while (fileCount < config.get("maxFileCount")) {
     const fileDirectory = getRandomValueFromArray(directories);
-    fakeFile.makeFile(getRandomSubDirectory(fileDirectory, config.get('dataRoot')), getRandomFileSize(config.get('fileSizeMin'), config.get('fileSizeMax'))); // fixed size or in between size
-    console.log(fileCount);
+    try {
+      await fakeFile.makeFile(
+        getRandomSubDirectory(fileDirectory, config.get("dataRoot")) + "/" + faker.system.fileName(),
+        getRandomFileSize(config.get("fileSizeMin"), config.get("fileSizeMax"))
+      ); // fixed size or in between size
+    } catch (e) {
+      console.log(chalk.red(e.message));
+    }
     fileCount++;
+    fileBar.update(fileCount);
+  }
 }
 
 // get a sub directory from a file path and optionally excluding a part of the path that can be used as a sub directory
-function getRandomSubDirectory(directoryPath, pathToExclude)
-{
-    let subDirs = directoryPath.split("/");
-    let subDirMin;
-    if(pathToExclude){
-        subDirMin = pathToExclude.split("/").length;
-    }
-    
-    let removedCount = 0;
-    let toRemoveCount = Math.random()*subDirs.length | 0;
+function getRandomSubDirectory(directoryPath, pathToExclude) {
+  let subDirs = directoryPath.split("/");
+  let subDirMin;
+  if (pathToExclude) {
+    subDirMin = pathToExclude.split("/").length;
+  }
 
-    if(subDirMin && toRemoveCount > subDirMin){
-        toRemoveCount = toRemoveCount - subDirMin;
-    }
+  let removedCount = 0;
+  let toRemoveCount = (Math.random() * subDirs.length) | 0;
 
-    while(removedCount < toRemoveCount){
-        subDirs.splice(-1);
-        removedCount++;
-    }
+  if (subDirMin && toRemoveCount > subDirMin) {
+    toRemoveCount = toRemoveCount - subDirMin;
+  }
 
-    let filePath = subDirs.join("/") + "/" + faker.system.fileName();
-    return filePath;
+  while (removedCount < toRemoveCount) {
+    subDirs.splice(-1);
+    removedCount++;
+  }
+
+  let filePath = subDirs.join("/");
+  return filePath;
 }
 
-function getRandomValueFromArray(array){
-    return array[Math.random()*array.length | 0];
+function getRandomValueFromArray(array) {
+  return array[(Math.random() * array.length) | 0];
 }
 
-function generateDirectoryPath(constructor, maxDepth)
-{
-    let depth = 1;
-    let path = "/" + getRandomValueFromArray(constructor);
-    while(depth < maxDepth){
-        path = path + "/" + getRandomValueFromArray(constructor);
-        depth++;
-    }
-    return path;
+function generateDirectoryPath(constructor, maxDepth) {
+  let depth = 1;
+  let path = "/" + getRandomValueFromArray(constructor);
+  while (depth < maxDepth) {
+    path = path + "/" + getRandomValueFromArray(constructor);
+    depth++;
+  }
+  return path;
 }
 
-function getRandomFileSize(min, max) {  
-    return Math.floor(
-        Math.random() * (max - min + 1) + min
-      );
+function getRandomFileSize(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
